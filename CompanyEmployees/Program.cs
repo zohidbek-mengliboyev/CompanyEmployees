a35e9e412fadc2de43bf12c0facc7c1f80ff5ac6
zohidbek <uzrivojcoder@gmail.com>
2024-02-09 07:26:42 +0400

    Handling GET Requests

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,4 +1,5 @@
 using CompanyEmployees.Extensions;
+using CompanyEmployees.Presentation;
 using Microsoft.AspNetCore.HttpOverrides;
 using NLog;
 
@@ -12,8 +13,10 @@ builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.AddAutoMapper(typeof(Program));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(AssemblyReference).Assembly);
 
 var app = builder.Build();
 
