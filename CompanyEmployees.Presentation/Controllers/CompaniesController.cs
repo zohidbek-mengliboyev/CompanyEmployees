130963d556729484665cdb2ab441258a08a71d6f
zohidbek <uzrivojcoder@gmail.com>
2024-02-09 08:47:37 +0400

    Getting Additional Resources

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -19,5 +19,12 @@ namespace CompanyEmployees.Presentation.Controllers
             return Ok(companies);
         }
 
+        [HttpGet("{id:guid}")] 
+        public IActionResult GetCompany(Guid id) 
+        { 
+            var company = _service.CompanyService.GetCompany(id, trackChanges: false); 
+            return Ok(company); 
+        }
+
     }
 }
