4165745478635c682a061f9d40b4301e613d19d3
zohidbek <uzrivojcoder@gmail.com>
2024-02-09 06:24:31 +0400

    Onion Architecture Implementation

---
+++ Repository/RepositoryManager.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+
+namespace Repository
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly RepositoryContext _context;
+        private readonly Lazy<ICompanyRepository> _companyRepository;
+        private readonly Lazy<IEmployeeRepository> _employeeRepository;
+
+        public RepositoryManager(RepositoryContext context)
+        {
+            _context = context;
+            _companyRepository = new Lazy<ICompanyRepository>(() => new CompanyRepository(context));
+            _employeeRepository = new Lazy<IEmployeeRepository>(() => new EmployeeRepository(context));
+        }
+        public ICompanyRepository Company => _companyRepository.Value;
+
+        public IEmployeeRepository Employee => _employeeRepository.Value;
+
+        public void Save() => _context.SaveChanges();
+    }
+}
