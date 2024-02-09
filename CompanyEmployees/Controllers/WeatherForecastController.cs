6926e5309cc50dc8d29df9f585b7cac2ffcde652
zohidbek <uzrivojcoder@gmail.com>
2024-02-09 04:35:13 +0400

    Configuring a Logging Service

--- CompanyEmployees/Controllers/WeatherForecastController.cs
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyEmployees.Controllers
@@ -12,15 +13,22 @@ namespace CompanyEmployees.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private ILoggerManager _loggerManager;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ILoggerManager loggerManager)
         {
             _logger = logger;
+            _loggerManager = loggerManager;
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _loggerManager.LogInfo("Here is info message from our values controller.");
+            _loggerManager.LogDebug("Here is debug message from our values controller."); 
+            _loggerManager.LogWarn("Here is warn message from our values controller."); 
+            _loggerManager.LogError("Here is an error message from our values controller.");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
