9c3bf3e437dc9d8b79c95fcb1d4c29b5e03ab060
zohidbek <uzrivojcoder@gmail.com>
2024-02-11 00:31:29 +0400

    Working with PUT requests

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -10,5 +10,6 @@ namespace Service.Contracts
         CompanyDto CreateCompany(CompanyForCreationDto company);
         (IEnumerable<CompanyDto> companies, string ids) CreateCompanyCollection(IEnumerable<CompanyForCreationDto> companyCollection);
         void DeleteCompany(Guid companyId, bool trackChanges);
+        void UpdateCompany(Guid companyid, CompanyForUpdateDto companyForUpdate, bool trackChanges);
     }
 }
